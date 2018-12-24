94dd4536af7bf6dd6981299541cbf35d2715ee7f
Kamadzy <kam4513@gmail.com>
2018-12-24 12:43:12 +0200

    Updates

---
+++ precache-manifest.c24ceff70a6c2bc007f337da5c2f50d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gergun/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2d9d8e2de5ad0fe6d438",
+    "url": "/gergun/static/js/runtime~main.2d9d8e2d.js"
+  },
+  {
+    "revision": "8afbc622ac2679d6c280",
+    "url": "/gergun/static/js/main.8afbc622.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/gergun/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "8afbc622ac2679d6c280",
+    "url": "/gergun/static/css/main.ca7ab315.chunk.css"
+  },
+  {
+    "revision": "7e51f4778d15b868d1d0de7c8982b307",
+    "url": "/gergun/index.html"
+  }
+];
